87012e45b72a9f8d15e24d87bab3c6a451569228
spazeddd <101244374+spazeddd@users.noreply.github.com>
2022-10-16 23:50:55 +0300

    ♡♡♡
    ♡♡♡

---
+++ doyoulovegod.py
@@ -0,0 +1,12 @@
+import webbrowser
+import string
+from colorama import init
+from colorama import Fore, Back, Style
+import time 
+import os
+init()
+print(Fore.GREEN)
+start1 = input("do you love god?")
+if start1 == "no":str
+webbrowser.open_new('https://www.youtube.com/watch?v=dRhed5i7-T4')
+#Оглавление для чайников:В командной строке пропиши no 
